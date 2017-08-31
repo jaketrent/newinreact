02caa8e3ec83fa7ddb455285a0c6d81a1e37c061
Anne Trent <anne.trent1@gmail.com>
2017-08-31 11:37:50 -0600

    add global styles

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Content from '../ui/content'
+import GlobalStyles from '../ui/global-styles'
 import Head from '../ui/head'
 import Header from '../ui/header'
 
 export default _ => (
   <div>
     <Head />
+    <GlobalStyles />
     <Content>
       <Header />
     </Content>
---
+++ ui/global-styles.js
@@ -0,0 +1,36 @@
+export default _ => (
+  <style jsx>{`
+    * {
+      box-sizing: border-box;
+    }
+    html,
+    body {
+      font-size: 16px;
+    }
+    h1,
+    h2,
+    h3 {
+    }
+    a {
+      text-decoration: none;
+      cursor: pointer;
+    }
+    p {
+      line-height: 30px;
+    }
+    @media screen and (min-width: 426px) {
+    }
+    @media screen and (min-width: 769px) {
+      html,
+      body {
+        font-size: 22px;
+      }
+    }
+    @media screen and (min-width: 1441px) {
+      html,
+      body {
+        font-size: 24px;
+      }
+    }
+  `}</style>
+)
