743a6a238388550f7c5ab3813cf695675422ce74
Anne Trent <anne.trent1@gmail.com>
2017-09-06 09:39:07 -0600

    add meta tags

--- ui/head.js
+++ ui/head.js
@@ -16,30 +16,22 @@ export default _ => (
     <meta name="MobileOptimized" content="320" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    {/* <meta name="description" content="" />
+    <meta name="description" content="New in React 16" />
     <meta
       name="keywords"
-      content=""
+      content="react, react 16, learn react, new in react, react error handling"
     />
     <meta name="copyright" content={`${now()} Jake Trent`} />
 
     <meta property="og:title" content="New in React" />
     <meta property="og:type" content="article" />
     <meta property="og:url" content="https://newinreact.com" />
-    <meta
-      property="og:description"
-      content=""
-    />
+    <meta property="og:description" content="New in React 16" />
 
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:title" content="New in React" />
-    <meta
-      name="twitter:description"
-      content=""
-    />
+    <meta name="twitter:description" content="New in React 16" />
     <meta name="twitter:site" content="@jaketrent" />
     <meta name="twitter:creator" content="@jaketrent" />
-
-    }*/}
   </Head>
 )
